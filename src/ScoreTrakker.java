d34eb8538311ddf3090aaaa85c8b8a350186948a
Stephen Agee <sagee@mines.edu>
2017-09-27 13:37:56 -0600

    Commit for part 2. Added try catch statements for opening file and parsing the score string for ints.

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -5,35 +5,63 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class ScoreTrakker {
-	private ArrayList <Student> students = new ArrayList <Student> ();
+	//arraylist of students
+	private ArrayList <Student> students;
+	private String files [] = {"scores.txt", "badscores.txt", "noscores.txt"};
 
-
-	public void loadDataFromFile (String name) throws FileNotFoundException{
-		FileReader reader = new FileReader(name);
+	public void loadDataFromFile (String fName) throws FileNotFoundException{
+		//make a new file reader
+		FileReader reader = new FileReader(fName);
+		//pass the file reader to a scanner
 		Scanner in = new Scanner(reader);
+
+		students = new ArrayList <Student> ();
 		
-		
-		
+		//read data from the file while it's not the end of the file, and store the information in student. 
+		//note: this will only work with a properly formatted file
 		while (in.hasNextLine()){
+			String name;
+			String score;
 			Student s = new Student();
-			s.setName(in.nextLine());
-			s.setScore(in.nextInt());
-			in.nextLine();
+			//get the name from the file
+			name = in.nextLine();
+			s.setName(name);
+			//get the score as next line
+			score = in.nextLine();
+			//try to parse the score to an int, if there is a number format exception, print the error and continue to the next line
+			try{
+				s.setScore(Integer.parseInt(score));
+			}
+			catch (NumberFormatException e){
+				System.out.println("Incorrect format for " + name + " not a valid score: " + score);
+				continue;
+			}
+
 			students.add(s);
 		}
 	}
 
 	public void printInOrder(){
+		//sort the students then print them out
 		Collections.sort(students);		
 		for (Student s: students){
 			System.out.println(s);
 		}
+		System.out.println();
 
 	}
 
 	public void processFiles() throws FileNotFoundException{
-		loadDataFromFile("scores.txt");
-		printInOrder();
+		for (String f : files){
+			try{
+				loadDataFromFile(f);
+				printInOrder();
+			}
+			catch (FileNotFoundException e){
+				System.out.println("Can't open file " + f);
+				continue;
+			}
+		}
 	}
 
 	public static void main (String [] args) throws FileNotFoundException{
