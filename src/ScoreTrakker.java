3325c7092a286635f7cd2fdd21795fcb22e7e109
Stephen Agee <sagee@mines.edu>
2017-09-27 13:06:35 -0600

    Added functionality for part 1. The project now takes in a text file, stores the data in student objects, orders the students by score, and prints them out to the screen

--- src/ScoreTrakker.java
+++ src/ScoreTrakker.java
@@ -5,13 +5,22 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class ScoreTrakker {
-	private ArrayList <Student> students;
+	private ArrayList <Student> students = new ArrayList <Student> ();
+
+
 	public void loadDataFromFile (String name) throws FileNotFoundException{
 		FileReader reader = new FileReader(name);
 		Scanner in = new Scanner(reader);
-		String temp = in.nextLine();
 		
-
+		
+		
+		while (in.hasNextLine()){
+			Student s = new Student();
+			s.setName(in.nextLine());
+			s.setScore(in.nextInt());
+			in.nextLine();
+			students.add(s);
+		}
 	}
 
 	public void printInOrder(){
@@ -19,6 +28,17 @@ public class ScoreTrakker {
 		for (Student s: students){
 			System.out.println(s);
 		}
+
+	}
+
+	public void processFiles() throws FileNotFoundException{
+		loadDataFromFile("scores.txt");
+		printInOrder();
+	}
+
+	public static void main (String [] args) throws FileNotFoundException{
+		ScoreTrakker st = new ScoreTrakker();
+		st.processFiles();
 
 	}
 }
--- src/Student.java
+++ src/Student.java
@@ -4,13 +4,22 @@ public class Student implements Comparable <Student>{
 	private int score;
 	
 	@Override
+	//compare the students by score, return positive if score passed in is greater
 	public int compareTo(Student s) {
 		return this.score - s.score;
 	}
 
 	@Override
 	public String toString() {
-		return name + score;
+		return name + " " + score;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
 	}
 	
 	
